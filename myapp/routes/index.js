67866e78d8ab0edc2f78d04a361f8450089bef8d
benzookapi <jokksk@gmail.com>
2016-11-12 09:01:07 +0900

    #

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -8,10 +8,8 @@ router.get('/', function(req, res, next) {
     accessToken: 'access_token$sandbox$btdxhpwfbt6dy2vt$41c6f24f692e018e2c68d84ce235fe79'
   });
   gateway.clientToken.generate({}, function (err, response) {
-    //res.send(response.clientToken);
-    res.render('index', { title: 'Express', clientToken: response.clientToken });
+    res.render('index', { clientToken: response.clientToken });
   });
-  //res.render('index', { title: 'Express' });
 });
 
 router.post("/checkout", function (req, res) {
@@ -23,6 +21,71 @@ router.post("/checkout", function (req, res) {
     amount: req.body.amount,
     merchantAccountId: "JPY",
     paymentMethodNonce: "" + req.body.payment_method_nonce,
+    options: {
+      submitForSettlement: true
+    }
+  };
+  gateway.transaction.sale(saleRequest, function (err, result) {
+    if (err) {
+      res.send("<h1>Error:  " + err + "</h1>");
+    } else if (result.success) {
+      res.send("<h1>Success! Transaction ID: " + result.transaction.id + "</h1>");
+    } else {
+      res.send("<h1>Error:  " + result.message + "</h1>");
+    }
+  });
+});
+
+/* ------- For Vault -------- */
+
+router.get('/vault', function(req, res, next) {
+  var gateway = braintree.connect({
+    accessToken: 'access_token$sandbox$btdxhpwfbt6dy2vt$41c6f24f692e018e2c68d84ce235fe79'
+  });
+  gateway.clientToken.generate({}, function (err, response) {
+    res.render('vault', { clientToken: response.clientToken });
+  });
+});
+
+router.post("/checkoutVault", function (req, res) {
+  // Use payment method nonce here
+  var gateway = braintree.connect({
+    accessToken: 'access_token$sandbox$btdxhpwfbt6dy2vt$41c6f24f692e018e2c68d84ce235fe79'
+　});
+  var saleRequest = {
+    amount: req.body.amount,
+    merchantAccountId: "JPY",
+    paymentMethodNonce: "" + req.body.payment_method_nonce,
+    deviceData: req.body.device,
+    options: {
+      submitForSettlement: true,
+      storeInVaultOnSuccess: true // For Vault
+    }
+  };
+  gateway.transaction.sale(saleRequest, function (err, result) {
+    if (err) {
+      res.send("<h1>Error:  " + err + "</h1>");
+    } else if (result.success) {
+      res.send("<h1>Success! Transaction ID: " + result.transaction.id + " Customer ID: " + result.transaction.customer.id + "</h1>");
+    } else {
+      res.send("<h1>Error:  " + result.message + "</h1>");
+    }
+  });
+});
+
+router.get('/vault_sale', function(req, res, next) {
+  res.render('vault_sale');
+});
+
+router.post("/vaultSale", function (req, res) {
+  // Use payment method nonce here
+  var gateway = braintree.connect({
+    accessToken: 'access_token$sandbox$btdxhpwfbt6dy2vt$41c6f24f692e018e2c68d84ce235fe79'
+　});
+  var saleRequest = {
+    amount: req.body.amount,
+    merchantAccountId: "JPY",
+    customerId: req.body.customerId,
     options: {
       submitForSettlement: true
     }
